bcda20c117e19cc67f1c0109402faf93eb43643c
G4GA <damian.gaga02@gmail.com>
2024-11-16 01:11:11 -0600

    Start server development

--- include/server.h
+++ include/server.h
@@ -1,4 +1,6 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+int init_server();
+
 #endif
--- src/server.c
+++ src/server.c
@@ -1,1 +1,23 @@
 #include "server.h"
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+//Internal functions
+int log_message(char *);
+
+int init_server()
+{
+    int rc = 0;
+
+    printf("Server initialization");
+    return rc;
+}
+
+//Internal function definitions
+int log_message(char *msg)
+{
+    printf("%s", msg);
+
+    int rc = 0;
+}
